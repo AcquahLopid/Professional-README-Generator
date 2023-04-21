26a6b15390f1a6cced386217601c4af8df410cc6
Acquah <emanuel.molina2k@gmail.com>
2023-04-20 17:57:04 -0700

    add requires for packages and README template

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile(fileName, data);
+}
 
 // Function call to initialize app
 init();
