521fa30e7c6c6bba7a1c27110d6a19a9c573c7d1
Acquah <emanuel.molina2k@gmail.com>
2023-04-20 21:03:37 -0700

    add questions array

--- index.js
+++ index.js
@@ -4,17 +4,44 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [{
-    
+    message : "title of your project:",
+    type : "input",
+    name : "title",
+},{
+    message : "Description of your project:",
+    type : "input",
+    name : "description",
+
+},{
+    message : "Your email:",
+    type : "input",
+    name : "email",
+},{
+    message : "License of your project:",
+    type : "input",
+    name : "title",
+},{
+    message : "Usage of your project:",
+    type : "list",
+    name : "license",
 }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeToFile(fileName);
+    text = generateMarkdown(data);
+    fs.appendFile(fileName, text, (err) =>
+        err ? console.log(err) : console.log("Successful!!!")
+            );
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile(fileName, data);
+    inquirer 
+    .createPromptModule(questions)
+    .then((data) => {
+        console.log(data);
+        const fileName = 'README_${}'
+    })
 }
 
 // Function call to initialize app
